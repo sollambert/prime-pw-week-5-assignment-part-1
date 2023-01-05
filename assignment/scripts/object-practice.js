716dd76be65bf5d7f588605fa69f64f71375437a
Solomon Lambert <sol.lambert@gmail.com>
2023-01-05 15:12:56 -0600

    log first and last fav foods

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -31,7 +31,8 @@ console.log(fullName);
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
 
 
 /* 4. Change a property of an existing object.
